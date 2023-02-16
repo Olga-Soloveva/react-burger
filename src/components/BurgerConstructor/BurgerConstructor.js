9b749578f7aadde5b825e7ebe44b3cbda180e03e
Olga Tovstaya <olga.tovstaya@yandex.ru>
2023-02-16 23:28:11 +0300

    feat: считать стоимость заказа

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,9 +1,6 @@
 import styles from "./burger-constructor.module.css";
-import PropTypes from "prop-types";
 import BurgerComponent from "../BurgerComponent/BurgerComponent";
-import React from "react";
-import { useState, useCallback, useMemo } from "react";
-import ingredientType from "../../utils/types";
+import React, { useState, useCallback, useContext } from "react";
 import { testDataOrder } from "../../utils/testData";
 import Modal from "../Modal/Modal";
 import OrderDetails from "../OrderDetails/OrderDetails";
@@ -11,23 +8,11 @@ import {
   Button,
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
+import { BurgerComponentContext } from "../../contexts/BurgerComponentContext";
 
-function BurgerConstructor({ components }) {
-  const bunComponent = useMemo(
-    () =>
-      components.find(function (component) {
-        return component.type === "bun";
-      }),
-    [components]
-  );
+function BurgerConstructor({ orderAmount }) {
+  const { bunComponent, otherComponent } = useContext(BurgerComponentContext);
 
-  const otherComponent = useMemo(
-    () =>
-      components.filter((component) => {
-        return component.type === "main" || component.type === "sauce";
-      }),
-    [components]
-  );
   const [isModalOrderOpen, setIsModalOrderOpen] = useState(false);
 
   const closeModal = useCallback(() => {
@@ -42,12 +27,12 @@ function BurgerConstructor({ components }) {
     <>
       <section className={`${styles.section_container} pt-25 `}>
         <div className={`${styles.burger_components}`}>
-          {bunComponent && (
+          {bunComponent.length !== 0 && (
             <div className={`${styles.components_container} pl-4 pr-4`}>
               <BurgerComponent component={bunComponent} type="top" />
             </div>
           )}
-          {otherComponent && (
+          {otherComponent.length !== 0 && (
             <div className={` ${styles.components_container_scrol} pl-4 pr-2`}>
               {otherComponent.map((component) => {
                 return (
@@ -56,7 +41,7 @@ function BurgerConstructor({ components }) {
               })}
             </div>
           )}
-          {bunComponent && (
+          {bunComponent.length !== 0 && (
             <div className={`${styles.components_container} pl-4 pr-4`}>
               <BurgerComponent component={bunComponent} type="bottom" />
             </div>
@@ -64,7 +49,7 @@ function BurgerConstructor({ components }) {
         </div>
         <div className={`${styles.info} mt-10`}>
           <div className={`${styles.price} mr-10`}>
-            <p className="text text_type_digits-medium">610</p>
+            <p className="text text_type_digits-medium">{orderAmount}</p>
             <CurrencyIcon type="primary" />
           </div>
           <Button
@@ -86,9 +71,5 @@ function BurgerConstructor({ components }) {
     </>
   );
 }
-
-BurgerConstructor.propTypes = {
-  components: PropTypes.arrayOf(ingredientType).isRequired,
-};
 
 export default React.memo(BurgerConstructor);
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,15 +1,97 @@
 import styles from "./main-content.module.css";
+import { useState, useMemo, useEffect, useReducer } from "react";
 import PropTypes from "prop-types";
 import ingredientType from "../../utils/types";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import BurgerIngedients from "../BurgerIngedients/BurgerIngedients";
+import { BurgerComponentContext } from "../../contexts/BurgerComponentContext";
 
-function MainContent({  ingredients }) {
+function MainContent({ ingredients }) {
+  const orderAmountInitialState = { orderAmount: 0 };
+  const [orderAmountState, orderAmountDispatcher] = useReducer(
+    reducer,
+    orderAmountInitialState,
+    undefined
+  );
+
+  const bunComponentData = useMemo(
+    () =>
+      ingredients.find(function (component) {
+        return component.type === "bun";
+      }),
+    [ingredients]
+  );
+
+  const otherComponentData = useMemo(
+    () =>
+      ingredients.filter((component) => {
+        return component.type === "main" || component.type === "sauce";
+      }),
+    [ingredients]
+  );
+
+  const [bunComponent, setBunComponent] = useState([]);
+  const [otherComponent, setOtherComponent] = useState([]);
+  const [order, setOrder] = useState([]);
+
+  useEffect(() => {
+    otherComponentData && setOtherComponent(otherComponentData);
+  }, [otherComponentData]);
+
+  useEffect(() => {
+    bunComponentData && setBunComponent(bunComponentData);
+  }, [bunComponentData]);
+
+  useEffect(() => {
+    setOrder([bunComponent, ...otherComponent]);
+  }, [bunComponent, otherComponent]);
+
+  useEffect(() => {
+    orderAmountDispatcher({
+      type: "reset",
+    });
+    order.forEach((component) => {
+      if (component.type === "bun") {
+        orderAmountDispatcher({
+          type: "addBunComponent",
+          payload: component.price,
+        });
+      } else if (component.type === "main" || component.type === "sauce") {
+        orderAmountDispatcher({
+          type: "addOtherComponent",
+          payload: component.price,
+        });
+      }
+    });
+  }, [order]);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "addBunComponent":
+        return { orderAmount: state.orderAmount + action.payload * 2 };
+      case "addOtherComponent":
+        return { orderAmount: state.orderAmount + action.payload };
+      case "delete":
+        return { orderAmount: state.orderAmount - action.payload };
+      case "reset":
+        return orderAmountInitialState;
+
+      default:
+        throw new Error(`Wrong type of action: ${action.type}`);
+    }
+  }
+
+  console.log(orderAmountState.orderAmount);
+
   return (
     <main className={styles.main_container}>
       <div className={styles.content}>
         <BurgerIngedients ingredients={ingredients} />
-        <BurgerConstructor components={ingredients} />
+        <BurgerComponentContext.Provider
+          value={{ bunComponent, otherComponent }}
+        >
+          <BurgerConstructor orderAmount={orderAmountState.orderAmount} />
+        </BurgerComponentContext.Provider>
       </div>
     </main>
   );
