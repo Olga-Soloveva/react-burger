d4a41c3cf682f8cbced197507430d118a299551e
Olga Tovstaya <olga.tovstaya@yandex.ru>
2023-03-05 19:08:10 +0300

    feat: сверстать страницу login и register

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,50 @@
 import styles from "./page.module.css";
+import AppHeader from "../components/AppHeader/AppHeader";
+import {
+  EmailInput,
+  PasswordInput,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
 
 export function LoginPage() {
   return (
     <div className={styles.page}>
-      <h1>Страница login</h1>
+      <AppHeader />
+      <div className={`${styles.content}`}>
+        <h1 className="text text_type_main-medium mb-6">Вход</h1>
+        <form className={`${styles.form_container}`}>
+          <EmailInput
+            // onChange={onChange}
+            // value={value}
+            name={"email"}
+            placeholder="E-mail"
+            isIcon={false}
+            extraClass="mb-6"
+          />
+          <PasswordInput
+            // onChange={onChange}
+            // value={value}
+            name={"password"}
+            extraClass="mb-6"
+          />
+          <Button htmlType="button" type="primary" size="medium">
+            Войти
+          </Button>
+        </form>
+        <p className="text text_type_main-default text_color_inactive mb-4">
+          Вы — новый пользователь?{" "}
+          <Link to="/register" className={styles.link}>
+            Зарегистрироваться
+          </Link>
+        </p>
+        <p className="text text_type_main-default text_color_inactive">
+          Забыли пароль?{" "}
+          <Link to="/forgot-password" className={styles.link}>
+            Восстановить пароль
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,9 +1,57 @@
 import styles from "./page.module.css";
+import AppHeader from "../components/AppHeader/AppHeader";
+import {
+  EmailInput,
+  PasswordInput,
+  Button,
+  Input,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
 
 export function RegisterPage() {
   return (
     <div className={styles.page}>
-      <h1>Страница register</h1>
+      <AppHeader />
+      <div className={`${styles.content}`}>
+        <h1 className="text text_type_main-medium mb-6">Регистрация</h1>
+        <form className={`${styles.form_container}`}>
+          <Input
+            type={"text"}
+            placeholder={"Имя"}
+            // onChange={e => setValue(e.target.value)}
+            // value={value}
+            name={"name"}
+            error={false}
+            errorText={"Ошибка"}
+            size={"default"}
+            extraClass="mb-6"
+          />
+          <EmailInput
+            // onChange={onChange}
+            // value={value}
+            name={"email"}
+            placeholder="E-mail"
+            isIcon={false}
+            extraClass="mb-6"
+          />
+          <PasswordInput
+            // onChange={onChange}
+            // value={value}
+            name={"password"}
+            extraClass="mb-6"
+          />
+          <Button htmlType="button" type="primary" size="medium">
+          Зарегистрироваться
+          </Button>
+        </form>
+        <p className="text text_type_main-default text_color_inactive mb-4">
+        Уже зарегистрированы?{" "}
+          <Link to="/login" className={styles.link}>
+            Войти
+          </Link>
+        </p>
+
+      </div>
     </div>
   );
 }
